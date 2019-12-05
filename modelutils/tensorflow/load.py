853ed96a9fd4033bec7f4e1eb371c7cd3cfa2911
Shohei Ono <>
2019-12-05 09:03:21 -0800

    TensorFlow load: SavedModel support.

--- modelutils/tensorflow/load.py
+++ modelutils/tensorflow/load.py
@@ -5,7 +5,7 @@ import tensorflow
 from google.protobuf import text_format
 
 
-def load(in_filename, out_filename):
+def load_graphdef(in_filename, out_filename):
     model = tensorflow.compat.v1.GraphDef()
     with open(in_filename, 'r') as f:
         text_format.Parse(f.read(), model)
@@ -13,6 +13,24 @@ def load(in_filename, out_filename):
     with open(out_filename, 'wb') as f:
         f.write(model.SerializeToString())
 
+
+def load_savedmodel(in_filename, out_filename):
+    from tensorflow.core.protobuf import saved_model_pb2
+    saved_model_def = saved_model_pb2.SavedModel()
+
+    with open(in_filename, 'r') as f:
+        text_format.Parse(f.read(), saved_model_def)
+
+    with open(out_filename, 'wb') as f:
+        f.write(saved_model_def.SerializeToString())
+
+
+def load(in_filename, out_filename):
+    try:
+        load_graphdef(in_filename, out_filename)
+    except:
+        print("Failed to load graphdef. Trying SavedModel format.")
+        load_savedmodel(in_filename, out_filename)
 
 def main():
     parser = argparse.ArgumentParser('Convert a text file to ONNX model')
