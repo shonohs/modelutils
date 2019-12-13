e7ff9f291b21f1bb07a3ef1c8da75692b05be027
Shohei Ono <>
2019-12-13 13:15:11 -0800

    onnx/convert_fp32_to_fp16: get input names from the model.

--- modelutils/onnx/convert_fp32_to_fp16.py
+++ modelutils/onnx/convert_fp32_to_fp16.py
@@ -4,13 +4,43 @@ import onnx
 import onnx.numpy_helper
 import numpy as np
 
+
+def get_input_names(model):
+    input_names = set(i.name for i in model.graph.input)
+    init_names = set(i.name for i in model.graph.initializer)
+    return list(input_names - init_names)
+
 def convert(input_filename, output_filename, exclude_tensor_names):
     if os.path.exists(output_filename):
         raise RuntimeError(f"{output_filename} already exists.")
 
     model = onnx.load(input_filename)
 
-    names = ['data']
+    names = get_input_names(model)
+    print(f"Detected input names: {names}")
+
+    # Convert Cast operators
+    for node in model.graph.node:
+        if node.op_type == 'Cast':
+            for attr in node.attribute:
+                if attr.name == 'to' and attr.i == onnx.TensorProto.FLOAT:
+                    attr.i = onnx.TensorProto.FLOAT16
+                    break
+
+    # Convert ConstantOfShape operators
+    for node in model.graph.node:
+        if node.op_type == 'ConstantOfShape':
+            default_tensor = onnx.helper.make_tensor('value', onnx.TensorProto.FLOAT16, [1], [1])
+            if not node.attribute:
+                node.attribute.append(onnx.helper.make_attribute('value', default_tensor))
+            else:
+                for attr in node.attribute:
+                    if attr.name == 'value' and attr.t.data_type == onnx.TensorProto.FLOAT:
+                        array = onnx.numpy_helper.to_array(attr.t)
+                        array = array.astype(np.float16)
+                        del attr.t.dims[:]
+                        del attr.t.float_data[:]
+                        attr.t.MergeFrom(onnx.numpy_helper.from_array(array, attr.t.name))
 
     # Convert initializers to FP16
     for i, initializer in enumerate(model.graph.initializer):
@@ -51,7 +81,7 @@ def main():
     parser = argparse.ArgumentParser('Quantize ONNX model to FP16')
     parser.add_argument('input_filepath', type=str)
     parser.add_argument('output_filepath', type=str)
-    parser.add_argument('--exclude_tensor', type=str, nargs='+', help="The tensor names which should not be converted")
+    parser.add_argument('--exclude_tensor', type=str, nargs='+', default=[], help="The tensor names which should not be converted")
 
     args = parser.parse_args()
 
