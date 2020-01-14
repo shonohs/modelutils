a32af1784cf6da6c5e715719815fcf03477ad5c4
shono <>
2020-01-14 13:39:59 -0800

    Add center_crop option to run-onnx.

--- modelutils/onnx/run.py
+++ modelutils/onnx/run.py
@@ -21,8 +21,21 @@ def add_output_node(input_model_filename, output_model_filename, output_node_nam
         f.write(model.SerializeToString())
 
 
-def preprocess_inputs(image_filename, input_shape, input_type, is_bgr=True, normalize_inputs=False, subtract_inputs=[]):
+def crop_image_center(image):
+    width, height = image.size
+    new_size = min(width, height)
+    left = (width - new_size) / 2
+    top = (height - new_size) / 2
+    right = left + new_size
+    bottom = top + new_size
+    return image.crop((left, top, right, bottom))
+
+
+def preprocess_inputs(image_filename, input_shape, input_type, is_bgr=True, normalize_inputs=False, subtract_inputs=[], center_crop=False):
     image = Image.open(image_filename)
+    if center_crop:
+        image = crop_image_center(image)
+    print(image.size)
     image = image.resize(input_shape, Image.ANTIALIAS)
     image = image.convert('RGB') if image.mode != 'RGB' else image
     image = np.asarray(image, dtype=np.float32)
@@ -47,7 +60,7 @@ def dump_outputs(name, shape, data):
         print('{}: {}'.format(i, data[i]))
 
 
-def run(model_filename, image_filename, output_names, normalize_inputs, subtract_inputs, is_bgr, enable_profiling, image_size):
+def run(model_filename, image_filename, output_names, normalize_inputs, subtract_inputs, is_bgr, enable_profiling, image_size, center_crop):
     session_options = onnxruntime.SessionOptions()
     session_options.enable_profiling = enable_profiling
 
@@ -64,7 +77,7 @@ def run(model_filename, image_filename, output_names, normalize_inputs, subtract
     image_shape = sess.get_inputs()[0].shape[2:]
     if image_size:
         image_shape = [int(image_size[0]), int(image_size[0])]
-    image = preprocess_inputs(image_filename, image_shape, sess.get_inputs()[0].type, is_bgr, normalize_inputs, subtract_inputs)
+    image = preprocess_inputs(image_filename, image_shape, sess.get_inputs()[0].type, is_bgr, normalize_inputs, subtract_inputs, center_crop)
 
     outputs = sess.run(output_names, {sess.get_inputs()[0].name: image})
 
@@ -88,11 +101,12 @@ def main():
     parser.add_argument('--normalize_inputs', action='store_true', help="Normalize the input to [0-1] range")
     parser.add_argument('--subtract_inputs', nargs='+', help="Subtract specified values from RGB inputs. ex) --subtract_inputs 123 117 104")
     parser.add_argument('--bgr', action='store_true', help="Use BGR instead of RGB")
+    parser.add_argument('--center_crop', action='store_true', help="Preprocess the input image by center cropping")
     parser.add_argument('--enable_profiling', action='store_true', default=False, help="Enable ONNX profiling")
     parser.add_argument('--image_size', nargs=1, help="Size of input image")
 
     args = parser.parse_args()
-    run(args.onnx_filename, args.image_filename, args.output_name, args.normalize_inputs, args.subtract_inputs, args.bgr, args.enable_profiling, args.image_size)
+    run(args.onnx_filename, args.image_filename, args.output_name, args.normalize_inputs, args.subtract_inputs, args.bgr, args.enable_profiling, args.image_size, args.center_crop)
 
 
 if __name__ == '__main__':
